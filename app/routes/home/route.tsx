ba885d861cf484ef2e163e90559fdd1da473f7c0
Nahuel Luca <nahuelluca20@gmail.com>
2024-01-24 09:04:26 -0300

    add dialog

--- app/routes/home/route.tsx
+++ app/routes/home/route.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+import { AlertDialog } from "~/ui/components/AlertDialog";
 import { Button } from "~/ui/components/Button";
+import { Modal } from "~/ui/components/Modal";
 
-export default function home() {
+export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div>
       home
-      <Button variant="destructive">HOLA</Button>
+      <Button variant="destructive" onPress={() => setModalOpen(!modalOpen)}>
+        HOLA
+      </Button>
+      <div>
+        <Button variant="secondary">Delete…</Button>
+        <Modal isOpen={modalOpen}>
+          <AlertDialog
+            actionLabel="Delete"
+            onAction={() => setModalOpen(!modalOpen)}
+            title="Delete folder"
+            variant="destructive"
+          >
+            Are you sure you want to delete {"Documents"}? All contents will be
+            permanently destroyed.
+          </AlertDialog>
+        </Modal>
+      </div>
     </div>
   );
 }
